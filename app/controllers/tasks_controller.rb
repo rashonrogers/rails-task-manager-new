597c26dbb63142c2e7213728edb26bb9c075a1a5
Rashon Rogers <rashonrogersj@gmail.com>
2023-06-29 15:34:41 +0900

    update and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,8 +7,35 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
   def create
-    @task = Task.new(params[:task])
-    @task.save
+    task = Task.new(params[:task])
+    task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
